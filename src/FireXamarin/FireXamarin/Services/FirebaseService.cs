f96dc78dfa4758ff9a3958d3c60fc60c6feaa64c
Eduardo <eduardo@forlogic.net>
2020-07-06 14:25:11 -0300

    criando service de conexao com firebase

--- src/FireXamarin/FireXamarin/Services/FirebaseService.cs
+++ src/FireXamarin/FireXamarin/Services/FirebaseService.cs
@@ -1,7 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BurgerMonkeys.Tools;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FireXamarin.Models;
+
 namespace FireXamarin.Services
 {
-    public class FirebaseService
+    public interface IFireBaseService
     {
-        
+        Task<IEnumerable<Contact>> GetAllContacts();
+        Task<Contact> GetById(string id);
+        Task<bool> AddContact(string name, string phone, string email);
+        Task<bool> EditContact(string id, string name, string phone, string email);
+        Task<bool> RemoveContact(string id);
+    }
+
+
+    public class FirebaseService : IFireBaseService
+    {
+        private readonly FirebaseClient _firebase = new FirebaseClient("https://xamarinfirebase-909d2.firebaseio.com/");
+        private const string Table = "Contacts";
+
+        public async Task<IEnumerable<Contact>> GetAllContacts()
+        {
+            return (await _firebase
+                .Child(Table)
+                .OnceAsync<Contact>()).Select(item =>
+                new Contact()
+                {
+                    Id = item.Object.Id,
+                    Name = item.Object.Name,
+                    Phone = item.Object.Phone,
+                    Email = item.Object.Email
+                });
+        }
+
+        public async Task<Contact> GetById(string id)
+        {
+            var result = (await _firebase
+                    .Child(Table)
+                    .OnceAsync<Contact>())
+                .FirstOrDefault(a => a.Object.Id == id);
+
+            return result?.Object;
+        }
+
+        public async Task<bool> AddContact(string name, string phone, string email)
+        {
+            var result = await _firebase
+                .Child(Table)
+                .PostAsync(
+                    new Contact()
+                    {
+                        Id = Generator.GetId(8),
+                        Name = name,
+                        Phone = phone,
+                        Email = email
+                    });
+
+            return result != null;
+        }
+
+        public async Task<bool> EditContact(string id, string name, string phone, string email)
+        {
+            var contactToEdit = (await _firebase
+                    .Child(Table)
+                    .OnceAsync<Contact>())
+                .FirstOrDefault(a => a.Object.Id == id);
+
+            try
+            {
+                await _firebase
+                    .Child(Table)
+                    .Child(contactToEdit?.Key)
+                    .PutAsync(new Contact()
+                    {
+                        Id = id,
+                        Name = name,
+                        Phone = phone,
+                        Email = email
+                    });
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> RemoveContact(string id)
+        {
+            var contactToRemove = (await _firebase
+                    .Child(Table)
+                    .OnceAsync<Contact>())
+                    .FirstOrDefault(a => a.Object.Id == id);
+
+            try
+            {
+                await _firebase.Child(Table)
+                    .Child(contactToRemove?.Key)
+                    .DeleteAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
     }
 }
